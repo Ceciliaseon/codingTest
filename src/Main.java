7a984704e5545a84801c39b83ff89be454159780
Ceciliaseon <mangseon2@gmail.com>
2024-07-15 12:15:13 +0900

    first codingTest

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        soltution(10, 5);
+        System.out.println(soltution(10, 5));
+    }
+
+    public static int soltution(int A, int B) {
+        int result = A - B;
+        return result;
+    }
+
+}
