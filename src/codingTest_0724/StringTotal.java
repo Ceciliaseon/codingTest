4b5545f08c103203118aaa18d5852fc1c92fc1c0
Ceciliaseon <mangseon2@gmail.com>
2024-07-29 12:01:11 +0900

    0725 codingTest

--- src/codingTest_0724/StringTotal.java
+++ src/codingTest_0724/StringTotal.java
@@ -51,12 +51,12 @@ public class StringTotal {
     // 문자열 my_string, overwrite_string과 정수 s가 주어집니다.
     // 문자열 my_string의 인덱스 s부터 overwrite_string의 길이만큼을
     // 문자열 overwrite_string으로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
-    public static void main(String[] args) {
-
-    }
-
     public String solution(String my_string, String overwrite_string, int s) {
-        String answer = "";
+        String answer = my_string.substring(0,s)+overwrite_string;
+
+        if(my_string.length() > answer.length()){
+            answer += my_string.substring(answer.length());
+        }
 
         return answer;
     }
